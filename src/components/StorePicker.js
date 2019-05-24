6f9b4e6251b23345bfbdff73448e7fe91813bdab
Umair Farooq <umair.farooq@osf-commerce.com>
2019-05-24 13:11:03 +0500

    Routing between store picker and app component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends Component {
+    constructor() {
+        super();
+        this.goToStore.bind(this);
+    }
+
+    storeIdInput = React.createRef();
+
+    goToStore = (event) => {
+        event.preventDefault();
+        this.props.history.push(`/store/${this.storeIdInput.current.value}`);
+    }
+
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please enter a store</h2>
-                <input type="text" required placeholder="Store Name" />
+                <input type="text"
+                    required
+                    placeholder="Store Name"
+                    defaultValue={getFunName()}
+                    ref={this.storeIdInput}
+                />
                 <button type="submit">Visit Store ></button>
             </form>
         );
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
-import StorePicker from './components/StorePicker';
-import App from './components/App';
+import Router from './components/Router';
 import './css/style.css';
 
 render(
-    <App />,
+    <Router />,
     document.getElementById('main')
 );
 
