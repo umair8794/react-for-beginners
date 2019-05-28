e4953bad0bddd6f544f02388e49de210bd3c595e
Umair Farooq <umair.farooq@osf-commerce.com>
2019-05-28 09:13:56 +0500

    Persisting order data in local storage

--- src/components/Order.js
+++ src/components/Order.js
@@ -7,6 +7,10 @@ class Order extends Component {
         const quantity = this.props.order[key];
         const isAvailable = fish && fish.status === "available";
 
+        if (!fish) {
+            return null;
+        }
+
         if (!isAvailable) {
             return <li key={key}>Sorry { fish ? fish.name : 'fish' } is no longer available</li>;
         }
